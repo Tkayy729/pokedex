c43122416b224298a19984749aa51b011940da4f
Emmanuel Koduah Tweneboah <laptop@Tkayys-MacBook-Pro.local>
2025-12-15 01:09:40 +0100

    fetch data

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,61 @@
 import React from "react";
 import "./App.css";
+import { pokemonData } from "../../data/pokemondata";
 import Pokedex from "./Pokedex/Pokedex";
+import {
+  PokemonSchema,
+  PokemonSpriteSchema,
+  UnpatchedPokemonSchema,
+} from "../types/PokemonSchema";
 
-class App extends React.Component {
+interface AppState {
+  searchField: string;
+  allPokemons: PokemonSchema[];
+  searchedPokemons: PokemonSchema[];
+  selectedPokemon: PokemonSchema | undefined;
+}
+
+class App extends React.Component<any, AppState> {
+  state = {
+    searchField: "",
+    allPokemons: [],
+    searchedPokemons: [],
+    selectedPokemon: undefined,
+  };
+
+  patchPokemonData = (pokemons: UnpatchedPokemonSchema[]): PokemonSchema[] => {
+    const patchedPokemons = pokemonData.map((pokemon) => {
+      let parsedSprites: PokemonSpriteSchema = {
+        normal: undefined,
+        animated: undefined,
+      };
+
+      try {
+        parsedSprites = pokemon.sprites && JSON.parse(pokemon.sprites);
+      } catch (e: any) {
+        console.log("Exception while parsing Sprites", e);
+      }
+
+      const patchedPokemon: PokemonSchema = {
+        ...pokemon,
+        sprites: parsedSprites,
+      };
+
+      return patchedPokemon;
+    });
+
+    return patchedPokemons;
+  };
+
+  componentDidMount(): void {
+    const patchedPokemons: PokemonSchema[] = this.patchPokemonData(pokemonData);
+
+    this.setState({
+      allPokemons: patchedPokemons,
+      searchedPokemons: patchedPokemons,
+    });
+  }
+
   render() {
     return (
       <div className="App">
